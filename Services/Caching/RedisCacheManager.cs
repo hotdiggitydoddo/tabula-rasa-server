034d1441fe4325b9650a34776b30112a30c1a7c9
Ryan Dodd <ryandodd@canvaspeople.com>
2018-10-19 17:27:29 -0700

    wip caching.

---
+++ Services/Caching/RedisCacheManager.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace TabulaRasa.Server.Services.Caching
+{
+    /// <summary>
+    /// Represents a manager for caching in Redis store (http://redis.io/).
+    /// Mostly it'll be used when running in a web farm or Azure.
+    /// But of course it can be also used on any server or environment
+    /// </summary>
+    public class RedisCacheManager : ICacheManager
+    {
+        #region Fields
+        private readonly IRedisConnectionWrapper _connectionWrapper;
+        private readonly IDatabase _db;
+        private readonly ICacheManager _perRequestCacheManager;
+
+        #endregion
+
+        #region Ctor
+
+        public RedisCacheManager(string connectionString, IRedisConnectionWrapper connectionWrapper, ICacheManager cacheManager)
+        {
+            // ConnectionMultiplexer.Connect should only be called once and shared between callers
+            this._connectionWrapper = connectionWrapper;
+            this._db = _connectionWrapper.GetDatabase();
+            this._perRequestCacheManager = cacheManager;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        protected byte[] Serialize(object item)
+        {
+            var jsonString = JsonConvert.SerializeObject(item);
+            return Encoding.UTF8.GetBytes(jsonString);
+        }
+        protected T Deserialize<T>(byte[] serializedObject)
+        {
+            if (serializedObject == null)
+                return default(T);
+
+            var jsonString = Encoding.UTF8.GetString(serializedObject);
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>The value associated with the specified key.</returns>
+        public T Get<T>(string key)
+        {
+            //little performance workaround here:
+            //we use "PerRequestCacheManager" to cache a loaded object in memory for the current HTTP request.
+            //this way we won't connect to Redis server 500 times per HTTP request (e.g. each time to load a locale or setting)
+            if (_perRequestCacheManager.IsSet(key))
+                return _perRequestCacheManager.Get<T>(key);
+
+            var rValue = _db.StringGet(key);
+            if (!rValue.HasValue)
+                return default(T);
+            var result = Deserialize<T>(rValue);
+
+            _perRequestCacheManager.Set(key, result, 0);
+            return result;
+        }
+
+        /// <summary>
+        /// Adds the specified key and object to the cache.
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="data">Data</param>
+        /// <param name="cacheTime">Cache time in minutes</param>
+        public void Set(string key, object data, int? cacheTime = null)
+        {
+            if (data == null)
+                return;
+
+            var entryBytes = Serialize(data);
+            TimeSpan? expiresIn = TimeSpan.FromMinutes(cacheTime.Value);
+
+            _db.StringSet(key, entryBytes, expiresIn);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the value associated with the specified key is cached
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>Result</returns>
+        public bool IsSet(string key)
+        {
+            //little performance workaround here:
+            //we use "PerRequestCacheManager" to cache a loaded object in memory for the current HTTP request.
+            //this way we won't connect to Redis server 500 times per HTTP request (e.g. each time to load a locale or setting)
+            if (_perRequestCacheManager.IsSet(key))
+                return true;
+
+            return _db.KeyExists(key);
+        }
+
+        /// <summary>
+        /// Removes the value with the specified key from the cache
+        /// </summary>
+        /// <param name="key">/key</param>
+        public void Remove(string key)
+        {
+            _db.KeyDelete(key);
+            _perRequestCacheManager.Remove(key);
+        }
+
+        /// <summary>
+        /// Removes items by pattern
+        /// </summary>
+        /// <param name="pattern">pattern</param>
+        public void RemoveByPattern(string pattern)
+        {
+            foreach (var ep in _connectionWrapper.GetEndPoints())
+            {
+                var server = _connectionWrapper.GetServer(ep);
+                var keys = server.Keys(database: _db.Database, pattern: "*" + pattern + "*");
+                foreach (var key in keys)
+                    Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// Clear all cache data
+        /// </summary>
+        public void Clear()
+        {
+            foreach (var ep in _connectionWrapper.GetEndPoints())
+            {
+                var server = _connectionWrapper.GetServer(ep);
+                //we can use the code below (commented)
+                //but it requires administration permission - ",allowAdmin=true"
+                //server.FlushDatabase();
+
+                //that's why we simply interate through all elements now
+                var keys = server.Keys(database: _db.Database);
+                foreach (var key in keys)
+                    Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public virtual void Dispose()
+        {
+            _connectionWrapper?.Dispose();
+        }
+
+        #endregion
+    }
+}
