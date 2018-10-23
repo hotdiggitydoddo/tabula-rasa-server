c1391773840be0f34ab23b5131353f80e3d2532d
Ryan Dodd <ryan@teletran-I.home>
2018-10-23 01:21:52 -0700

    wip

---
+++ Services/ScriptService.cs
@@ -0,0 +1,17 @@
+using MoonSharp.Interpreter;
+
+namespace TabulaRasa.Server.Services
+{
+    public class ScriptService : IScriptService
+    {
+        public Script GetScript(ScriptType type, string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Init()
+        {
+            return true;
+        }
+    }
+}
